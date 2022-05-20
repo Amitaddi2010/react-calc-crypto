e62ae324f1e9437c1d772c3e87bb913263986060
Amit Raj Saraswat <Amit.addi2010@gmail.com>
2022-05-20 15:12:51 +0530

    initial

---
+++ src/components/BlockMan.js
@@ -0,0 +1,35 @@
+import React from 'react'
+
+export const BlockMan = () => {
+  return (
+    <div class="card text-center">
+    <div class="card">
+  <div class="card-header">
+    Featured
+  </div>
+  <div class="card-body">
+    <h5 class="card-title">Blockchain</h5>
+    <p class="card-text">Blockchain is a type of shared database that differs from a typical database in the way that it stores information.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+  <hr/>
+  <div class="card-body">
+    <h5 class="card-title">Metaverse</h5>
+    <p class="card-text">Blockchain is a type of shared database that differs from a typical database in the way that it stores information.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+  <hr/>
+  <div class="card-body">
+    <h5 class="card-title">NFT</h5>
+    <p class="card-text">Blockchain is a type of shared database that differs from a typical database in the way that it stores information.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+    
+  </div>
+</div>
+
+</div>
+
+
+
+  )
+}
